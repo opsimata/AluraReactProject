d6bfef28c4b40b3af9ccc8a92b558fc64e6464f0
Pedro <pedro.enrique1599@icloud.com>
2023-02-01 00:46:23 -0300

    Commit dos aquivos realizados no curso

---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,23 @@
+import style from "./Relogio.module.scss";
+
+interface Props {
+    tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0 }: Props){
+
+    const minutos = Math.floor(tempo/60);
+    const segundos = tempo % 60;
+    const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+    const [SegundoDezena, SegundoUnidade] = String(segundos).padStart(2, '0');
+
+    return(
+        <>
+            <span className={style.relogioNumero}>{minutoDezena}</span>
+            <span className={style.relogioNumero}>{minutoUnidade}</span>
+            <span className={style.relogioDivisao}>:</span>
+            <span className={style.relogioNumero}>{SegundoDezena}</span>
+            <span className={style.relogioNumero}>{SegundoUnidade}</span>
+        </>
+    )
+}
